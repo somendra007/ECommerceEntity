e86ace6c009bbf10b84709a4b5a456cf3ca00b65
Somendra Pratap Singh <somenprtap.singh@gmail.com>
2021-09-17 01:18:43 +0530

    Add project files.

---
+++ EcommerceEntity/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EcommerceEntity.Models
+{
+    internal class Customer
+    {
+        public Guid CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ EcommerceEntity/Models/ECommerceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceEntity.Models
+{
+    internal class ECommerceModel
+    {
+        public ECommerceModel():base()
+        {
+
+        }
+    }
+}
